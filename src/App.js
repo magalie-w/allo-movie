5189ba688c0fced95b77f587d7acf426b010b110
magalie <magalie.waligora@gmail.com>
2023-01-04 16:38:16 +0100

    api movie

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import Movies from './Movies.js';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="bg-yellow-500 flex justify-between px-5 py-3">
+        <div>
+          <h1>AlloMovie</h1>
+        </div>
+
+        <div className="flex justify-around w-[200px]">
+          <h2>Home</h2>
+          <h2>Favoris</h2>
+        </div>
+      </div>
+
+    <div>
+      <Movies />
+    </div>
     </div>
   );
 }
---
+++ src/Movies.js
@@ -0,0 +1,56 @@
+import { Component } from "react";
+
+class Movies extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            error: null,
+            isLoaded: false,
+            movies: [],
+            secur_url: null,
+            size: [],
+        };
+    }
+
+    componentDidMount() {
+
+        // API des films
+        fetch("https://api.themoviedb.org/3/discover/movie?api_key=a4872679cc543a16475400119b475fb6&language=fr-FR&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate")
+        .then(res => res.json())
+        .then(
+            (result) => {
+                this.setState({
+                    isLoaded: true,
+                    movies: result.results
+                });
+            },
+
+            (error) => {
+                this.setState({
+                    isLoaded: true,
+                    error
+                });
+            }
+        )
+    }
+
+    render() {
+        const secur_url = "https://image.tmdb.org/t/p";
+        const size = "original";
+
+        return (
+            <div class="flex flex-wrap">
+                {this.state.movies.map(movie => (
+                    <div key={movie.id} class="w-[500px] mx-auto flex border">
+                        <img src={secur_url + "/" + size + movie.poster_path} alt="" width="200"/>
+                        <h1>{movie.title}</h1>
+                    </div>
+                ))}
+            </div>
+        )
+    
+    }
+}
+
+export default Movies;
